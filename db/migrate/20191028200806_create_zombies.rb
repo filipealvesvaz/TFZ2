aa7f60c2306044d11ed5f7ee2828a61db4fb5e43
EC2 Default User <ec2-user@ip-172-31-89-139.ec2.internal>
2019-10-28 20:11:24 +0000

    backup

---
+++ db/migrate/20191028200806_create_zombies.rb
@@ -0,0 +1,10 @@
+class CreateZombies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :zombies do |t|
+      t.string :name
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
